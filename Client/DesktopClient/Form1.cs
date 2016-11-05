c87bba24b7cf9c8a3dc679e6ffa06f3f6685a6ec
serwan <serwan91@gmail.com>
2016-11-06 01:13:30 +0530

    first build

---
+++ Client/DesktopClient/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using DesktopClient.WCFServiceDemo;
+
+namespace DesktopClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WCFServiceDemo.Service1Client service = new WCFServiceDemo.Service1Client(comboBox1.SelectedItem.ToString());
+            MessageBox.Show(service.GetMessage(textBox1.Text));
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("WSHttpBindingDemo");
+            comboBox1.Items.Add("BasicHttpBindingDemo");
+            comboBox1.Items.Add("NetTcpBindingDemo");
+        }
+    }
+}
--- WCFDemoService/Service1.cs
+++ WCFDemoService/Service1.cs
@@ -10,22 +10,9 @@ namespace WCFDemoService
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public string GetMessage(string name)
         {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return String.Format("Hello From WCF Service, {0}", name);
         }
     }
 }
