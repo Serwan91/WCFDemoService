229b0ec7e4f60b6d7cb4f2d92f477f8ac3957125
serwan <serwan91@gmail.com>
2016-11-06 14:40:30 +0530

    2nd commit

--- WCFDemoService/IService1.cs
+++ WCFDemoService/IService1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 
 namespace WCFDemoService
@@ -12,6 +13,9 @@ namespace WCFDemoService
     public interface IService1
     {
         [OperationContract]
+        [WebInvoke(UriTemplate = "/GetMessage/{name}", Method = "GET", 
+            BodyStyle = WebMessageBodyStyle.WrappedRequest, 
+            ResponseFormat = WebMessageFormat.Json)]
         string GetMessage(string name);
     }
 }
